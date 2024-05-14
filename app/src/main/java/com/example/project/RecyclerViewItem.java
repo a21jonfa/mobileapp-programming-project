c51738ba54fd682622e699c9dbd824c3e7a5857f
Jonas <jfand125@gmail.com>
2024-05-14 17:53:13 +0200

    About activity added, recyclerview implemented

---
+++ app/src/main/java/com/example/project/RecyclerViewItem.java
@@ -0,0 +1,13 @@
+package com.example.project;
+
+public class RecyclerViewItem {
+    String ID;
+
+    public RecyclerViewItem(String ID) {
+        this.ID = ID;
+    }
+
+    public String getID() {
+        return ID;
+    }
+}
